01d89dca9692ef6b5b0e0d8d18af5dff8551c95e
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-10-14 16:38:07 +0530

    resolve errors

---
+++ src/app/product/women/tops/page.tsx
@@ -0,0 +1,20 @@
+
+'use client'
+import MainLayout from "@components/layout/MainLayout";
+import React from "react";
+import ProductList from '../../../../../components/ProductList';
+import '../../../../app/globals.css'
+
+
+export default function skirts () {
+    return (
+     <MainLayout> 
+     <ProductList
+     category="women"
+     order=">="
+     limit={12}
+     group='Brand'
+     type='tops'
+     /></MainLayout>
+    )
+}
