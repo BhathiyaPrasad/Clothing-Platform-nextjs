cd74907ecbfa794cc492976bd202cb536baae51b
Bhathiya Prasad <90783913+BhathiyaPrasad@users.noreply.github.com>
2024-10-15 17:23:09 +0530

    component

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import '../Styles/header.css';
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-import SaluniLogo from '../../assests/images/final1.2.jpg';
+import SaluniLogo from '../../assests/images/final1.3.jpg';
 
 const Header = () => {
   const [items, setItems] = useState([]);
@@ -19,7 +19,7 @@ const Header = () => {
 
   return (
     <>
-      <div className="sticky top-0 z-50 bg-white shadow-md">
+      <div className="sticky top-0 z-50 bg-black shadow-md text-white ">
         {/* Top Announcement Bar */}
         <div className="bg-gray-200 text-center py-2 text-sm text-gray-900 font-black">ISLAND WIDE CASH ON DELIVERY</div>
 
@@ -33,7 +33,7 @@ const Header = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                 </svg>
               </div>
-              <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-black">
                 <li><Link href="/">HOME</Link></li>
                 <li>
                   <details>
@@ -114,7 +114,7 @@ const Header = () => {
             {/* Cart Button */}
             <div className="relative">
               <Link href="/product/cart">
-                <p className="text-gray-700 hover:text-black">
+                <p className="text-white-700 hover:text-black">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                   </svg>
